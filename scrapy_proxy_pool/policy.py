b833f215288d451a620fa18bfd542e073b5be19f
QiuLin Wang <qwang16@olivetuniversity.edu>
2020-03-04 10:06:16 -0800

    Upgrade BanDetectionPolicy.

--- scrapy_proxy_pool/policy.py
+++ scrapy_proxy_pool/policy.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+import re
+
 from scrapy.exceptions import IgnoreRequest
 
 
@@ -6,12 +9,17 @@ class BanDetectionPolicy(object):
     """ Default ban detection rules. """
     NOT_BAN_STATUSES = {200, 301, 302, 404, 500}
     NOT_BAN_EXCEPTIONS = (IgnoreRequest,)
+    BANNED_PATTERN = re.compile(r'(Captive Portal|SESSION EXPIRED)', re.IGNORECASE)
 
     def response_is_ban(self, request, response):
+        if self.BANNED_PATTERN.search(response.text):
+            return True
+
         if response.status not in self.NOT_BAN_STATUSES:
             return True
         if response.status == 200 and not len(response.body):
             return True
+
         return False
 
     def exception_is_ban(self, request, exception):
